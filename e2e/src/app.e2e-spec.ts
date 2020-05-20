573a728af849e3d8b13ced031fcf78fa0a4a5786
Daniel WOAGOU <daniwx@gmail.com>
2020-05-20 16:47:33 +0000

    Mise correction publication

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('MyApp app is running!');
+    expect(page.getTitleText()).toEqual('angular-heroku app is running!');
   });
 
   afterEach(async () => {
--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/tpTechweb'));
+app.use(express.static('./dist/angular-heroku'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/tpTechweb/'}),
+    res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
 );
 
 app.listen(process.env.PORT || 8080);
